ebc50f0a110f8d4ca41e1cafa76bf4ecdcdfc096
vinigrzyb <vinicius.grzyb@gmail.com>
2023-09-02 14:40:01 -0300

    INPUT

--- index.js
+++
@@ -1,9 +0,0 @@
-const livros = require("./lista-livros");
-let maisBarato = 0
-
-for (let atual = 0; atual < livros.length; atual++) {
-    if (livros[atual] < livros[maisBarato]) {
-        maisBarato = atual
-    }
-}
-console.log(`O livro masi barato custa ${livros[maisBarato]}`);
